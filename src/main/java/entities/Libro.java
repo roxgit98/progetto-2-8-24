2436d6bd83d66768349e13c1d891faf0dc92c123
Davide Giliberti <gilo.davide@hotmail.it>
2024-08-02 15:08:08 +0200

    Libro modificato

--- src/main/java/entities/Libro.java
+++ src/main/java/entities/Libro.java
@@ -4,26 +4,27 @@ public class Libro extends Catalogo {
     private String autore;
     private String genere;
 
-
-    public Libro(String titolo, int annoPubblicazione, int numeroPagine, String autore, String genere) {
-        super(titolo, annoPubblicazione, numeroPagine);
+    public Libro(String codiceIsbn, String titolo, int annoPubblicazione, int numeroPagine, String autore, String genere) {
+        super(codiceIsbn, titolo, annoPubblicazione, numeroPagine);
         this.autore = autore;
         this.genere = genere;
     }
+
 
     public String getAutore() {
         return autore;
     }
 
-    public void setAutore(String autore) {
-        this.autore = autore;
-    }
 
     public String getGenere() {
         return genere;
     }
 
-    public void setGenere(String genere) {
-        this.genere = genere;
+    @Override
+    public String toString() {
+        return "Libro{" +
+                "autore='" + autore + '\'' +
+                ", genere='" + genere + '\'' +
+                "} " + super.toString();
     }
 }
